ffc0ce6e57316ac115f06a130c7bff3080641aac
L3D <l3d@c3woc.de>
2021-10-03 18:07:26 +0200

    update initial filter

--- lektor_image_filter.py
+++ lektor_image_filter.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 from lektor.pluginsystem import Plugin
+from lektor.context import get_ctx
 
+def cfg():
+    ctx = get_ctx()
+    plugin = ctx.env.plugins["image-resize"]
+    config = plugin.config
+    return config
+
+
+#        for item, conf in config.items():
+#            width = int(conf["max_width"])
+#            height = int(conf.get("max_height", "0"))
+
+
+def image_filter(inputstring):
+    config = cfg()
+    return str(inputstring + "foo" + str(config.items()))
 
 class ImageFilterPlugin(Plugin):
     name = 'image-filter'
-    description = u'Add your description here.'
+    description = u'A filter to print the input image in different predefined image sizes.'
 
-    def on_process_template_context(self, context, **extra):
-        def test_function():
-            return 'Value from plugin %s' % self.name
-        context['test_function'] = test_function
+    def on_setup_env(self, **extra):
+        self.env.jinja_env.filters['webpimagesizes'] = image_filter
+
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_image_filter'],
     # url='[link to your repository]',
-    version='0.1',
+    version='0.2',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
